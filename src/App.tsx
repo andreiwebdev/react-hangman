e465a2abdc33ef476fd72460d0f5aa9f15e47443
Andrei <dandrei.web@gmail.com>
2022-11-28 17:15:32 +0200

    project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import words from "./wordList.json";
 import "./index.css";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -9,7 +12,14 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  return <div></div>;
+  return (
+    <div className="max-w-[800px] flex flex-col space-x-4 mx-auto items-center">
+      <div className="text-2lg text-center">Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
