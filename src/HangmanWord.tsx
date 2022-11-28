ca1f4863183429a9bc1edba09a5c2cb3aca85d51
Andrei <dandrei.web@gmail.com>
2022-11-28 19:23:09 +0200

    Update HangmanWord.tsx

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,24 @@
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetters = ["e", "t"];
+
+  return (
+    <div className="flex space-x-2 text-8xl font-bold uppercase font-mono">
+      {word.split("").map((letter, index) => (
+        <span key={index} className="border-b-4 border-black">
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
